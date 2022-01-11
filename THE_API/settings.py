4ebf7eb0cbb11a46cfa513684f71fd59ad0d3381
robert0628 <robertcarnesdev@gmail.com>
2022-01-11 17:08:10 -0500

    updated requirements.txt and added a user example for modelview set

--- THE_API/settings.py
+++ THE_API/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'THE_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dev_THE_TMS',
+        'USER': 'thisguy',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -100,6 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Internationalization
