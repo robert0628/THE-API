675685394cabf37a3e86f4ab1669404881c9cbc2
robert0628 <robertcarnesdev@gmail.com>
2022-01-12 11:54:23 -0500

    created the Driver model

--- THE_API/settings.py
+++ THE_API/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tindall_haul_erect',
+    'phonenumber_field',
+    'localflavor',
     'rest_framework',
 ]
 
--- tindall_haul_erect/models.py
+++ tindall_haul_erect/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from localflavor.us.models import USStateField, USZipCodeField, USSocialSecurityNumberField
+
 
 # Create your models here.
+"""
+Tractor #	
+Driver	
+LicNo	
+LicSt	
+Lic Exp	
+Domicile Location	
+DOB	Soc Sec #	
+Hire Date	
+Emp #	
+Phone #	
+Email Address	
+Home Address	
+Mailing Street Address	
+City and State	
+Zip Code	
+Emergency Contact	
+Phone #	2nd Contact
+"""
+
+
+class Driver(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    date_of_birth = models.DateField(blank=False)
+    tractor_num = models.CharField(max_length=255, blank=False)
+    license_num = models.CharField(max_length=255, blank=False)
+    license_state = models.CharField(max_length=255, blank=False)
+    license_exp = models.DateField(blank=False)
+    domicile_location = models.CharField(max_length=255, blank=False)
+    social_security_num = USSocialSecurityNumberField(blank=False)
+    hire_date = models.DateField(blank=False)
+    employee_num = models.CharField(max_length=255, blank=False, unique=True)
+    primary_phone_num = PhoneNumberField(blank=False, unique=True)
+    email_address = models.EmailField(max_length=255, blank=False, unique=True)
+    home_address = models.CharField(max_length=255, blank=False)
+    mailing_address = models.CharField(max_length=255, blank=False)
+    city = models.CharField(max_length=100, blank=False)
+    state = USStateField(blank=False)
+    zip_code = USZipCodeField(blank=False)
+    emergency_contact_name = models.CharField(max_length=100, blank=False)
+    emergency_contact_phone_num = PhoneNumberField(blank=False)
+
+
+
+
+
+
